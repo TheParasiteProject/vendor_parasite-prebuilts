25670a0128521e79f1eccca17738cc1eb2049cdf
someone5678 <someone5678@users.noreply.github.com>
2024-06-03 19:43:53 +0900

    parasite-prebuilts: Prepare for import microG
    * As our project's features relies on GMS,
      these will only override PrebuiltGmsCore and GoogleServicesFramework
      while shipping other GMS components as usual.
    
    https://github.com/microg
    
    Change-Id: I894594ce59a750c4da13d02263105999f12e994f

--- config.mk
+++ config.mk
@@ -4,3 +4,22 @@ ifeq ($(TARGET_INCLUDE_GRAMOPHONE),true)
 PRODUCT_PACKAGES += \
     Gramophone
 endif
+
+# microG
+ifeq ($(TARGET_INCLUDE_MICROG),true)
+PRODUCT_PACKAGES += \
+    GmsCore \
+    GsfProxy
+
+PRODUCT_PACKAGES += \
+    com.google.android.maps
+
+PRODUCT_PACKAGES += \
+    microGConfigOverlay
+
+PRODUCT_COPY_FILES += \
+    vendor/parasite-prebuilts/common/microG/common/product/etc/default-permissions/permissions-com.google.android.gms.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/permissions-com.google.android.gms.xml \
+    vendor/parasite-prebuilts/common/microG/common/product/etc/permissions/com.google.android.maps.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.maps.xml \
+    vendor/parasite-prebuilts/common/microG/common/product/etc/permissions/privapp-permissions-org.microG.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-org.microG.xml \
+    vendor/parasite-prebuilts/common/microG/common/product/etc/sysconfig/org.microG.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/org.microG.xml
+endif
