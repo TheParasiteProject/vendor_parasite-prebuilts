e60146b581703eba2759be0641af67d59782f995
someone5678 <someone5678@users.noreply.github.com>
2024-06-10 23:45:06 +0900

    prebuilts: update-files: Add Accord
    Change-Id: Iaa59738c0b030ff5f4e7a4ab50a57d9ef2fe5a82

--- update-files.sh
+++ update-files.sh
@@ -34,6 +34,13 @@ function getAkaneFoundationApks() {
     # Akane Foundation
     local akane_fdn_dir=$COMMON_DIR/AkaneFoundation
 
+    # Accord
+    local acc_name=AkaneFoundation
+    local app_name=Accord
+    local app_file='Accord.*.apk'
+    local target_dir="$akane_fdn_dir/common/product/app/$app_name"
+    getApkFromOnline $acc_name $app_name $app_name $app_file $app_name $target_dir
+
     # Gramophone
     local acc_name=AkaneTan
     local app_name=Gramophone
